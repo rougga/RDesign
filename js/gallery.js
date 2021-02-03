a0952add13e64096a298badf00ed722f5583e593
xTwentyFour <bougado.youssef@hotmail.com>
2021-02-03 22:08:26 +0100

    rougga

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,13 @@
 let intItems = [
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-1.webp',
-        w: 2600,
-        h: 1585
+        w: 2000,
+        h: 1220
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-2.webp',
-        w: 2000,
-        h: 1220
+        w: 2132,
+        h: 1300
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-3.webp',
@@ -21,124 +21,119 @@ let intItems = [
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-5.webp',
-        w: 2000,
-        h: 1220
+        w: 2132,
+        h: 1300
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-6.webp',
-        w: 2000,
-        h: 1220
-    },
-    {
-        src: 'http://rougga.ma/img/gallery/int/g-int-7.webp',
-        w: 2000,
-        h: 1220
+        w: 2132,
+        h: 1300
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-8.webp',
-        w: 2000,
-        h: 1220
+        w: 2500,
+        h: 1524
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-9.webp',
         w: 2000,
-        h: 1220
+        h: 1500
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-10.webp',
-        w: 2000,
-        h: 1220
+        w: 1600,
+        h: 976
     },
     {
         src: 'http://rougga.ma/img/gallery/int/g-int-11.webp',
-        w: 2000,
-        h: 1220
+        w: 2132,
+        h: 1300
     }
 ];
 
 let extItems = [
     {
         src: 'http://rougga.ma/img/gallery/ext/g-ext-1.webp',
-        w: 2600,
-        h: 1711
+        w: 2000,
+        h: 1500
     }, 
     {
         src: 'http://rougga.ma/img/gallery/ext/g-ext-2.webp',
-        w: 2600,
-        h: 1711
+        w: 1500,
+        h: 1150
     }, 
     {
         src: 'http://rougga.ma/img/gallery/ext/g-ext-3.webp',
-        w: 2600,
-        h: 1711
+        w: 1500,
+        h: 1150
     }, 
     {
         src: 'http://rougga.ma/img/gallery/ext/g-ext-4.webp',
-        w: 2600,
-        h: 1711
+        w: 1280,
+        h: 842
     },
     {
         src: 'http://rougga.ma/img/gallery/ext/g-ext-5.webp',
-        w: 2600,
-        h: 1711
+        w: 1280,
+        h: 842
     },
     {
         src: 'http://rougga.ma/img/gallery/ext/g-ext-6.webp',
-        w: 2600,
-        h: 1711
+        w: 1280,
+        h: 842
     }
 ];
 
 let autItems = [
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-1.webp',
-        w: 2600,
-        h: 1711
+        w: 1280,
+        h: 780
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-2.webp',
-        w: 2600,
-        h: 1711
+        w: 1280,
+        h: 780
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-3.webp',
         w: 2600,
-        h: 1711
+        h: 1585
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-4.webp',
-        w: 2600,
-        h: 1711
+        w: 2000,
+        h: 1043
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-5.webp',
-        w: 2600,
-        h: 1711
+        w: 1600,
+        h: 1300
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-6.webp',
-        w: 2600,
-        h: 1711
+        w: 1600,
+        h: 1300
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-7.webp',
         w: 2600,
-        h: 1711
+        h: 1585
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-8.webp',
         w: 2600,
-        h: 1711
+        h: 1585
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-9.webp',
-        w: 2600,
-        h: 1711
+        w: 1280,
+        h: 780
     },
     {
         src: 'http://rougga.ma/img/gallery/aut/g-aut-10.webp',
-        w: 2600,
-        h: 1711
+        w: 1280,
+        h: 780
     }
     
 ];
@@ -146,22 +141,22 @@ let autItems = [
 let p2dItems = [
     {
         src: 'http://rougga.ma/img/gallery/p2d/g-p2d-1.webp',
-        w: 2600,
-        h: 1711
+        w: 3113,
+        h: 4161
     },
     {
         src: 'http://rougga.ma/img/gallery/p2d/g-p2d-2.webp',
-        w: 2600,
-        h: 1711
+        w: 3193,
+        h: 3968
     },
     {
         src: 'http://rougga.ma/img/gallery/p2d/g-p2d-3.webp',
-        w: 2600,
-        h: 1711
+        w: 3193,
+        h: 3968
     },
     {
         src: 'http://rougga.ma/img/gallery/p2d/g-p2d-4.webp',
-        w: 2600,
-        h: 1711
+        w: 3308,
+        h: 2269
     }
 ];
