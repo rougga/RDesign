1a2d921f6c62fbeaa35279dbc1c85cee33a1a426
xTwentyFour <bougado.youssef@hotmail.com>
2021-02-04 19:42:18 +0100

    change to vertical images in mobile view

--- js/script.js
+++ js/script.js
@@ -16,6 +16,13 @@ $(document).ready(function () {
         }
     });
 
+    if(window.innerHeight>window.innerWidth){
+        let arr = $(".bg") ;
+        for ( let i=0;i<arr.length;i++ ) {
+            $(arr[i]).attr("data-src" ,"img/bg/webp/tele/bg-8.webp");
+        }
+    }
+
     $("#int").click(function () {
         openPS(intItems);
     });
